fa09749bf35dc69364b86bc47f6f30268d5b552f
Andy Weiss <andyweiss1982@gmail.com>
2020-08-26 14:18:30 -0600

    ability to add new task

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import Task from "./components/Task";
 
 const App = () => {
   const [tasks, setTasks] = React.useState([]);
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const [taskDescription, setTaskDescription] = React.useState("");
 
   const fetchTasks = async () => {
     const url = `/tasks`;
@@ -12,38 +14,83 @@ const App = () => {
     setTasks(data);
   };
 
+  const handleModalClick = (event) => {
+    const wasTheClickOutsideTheForm = !event.target.closest("form");
+    if (wasTheClickOutsideTheForm) {
+      setIsModalOpen(false);
+    }
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const url = `/tasks`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ description: taskDescription }),
+    });
+    const newTask = await response.json();
+    setTasks([newTask, ...tasks]);
+    setTaskDescription("");
+    setIsModalOpen(false);
+  };
+
   React.useEffect(() => {
     fetchTasks();
   }, []);
 
   return (
-    <main>
-      <section id="controls">
-        <div>
-          <label htmlFor="status">Status</label>
-          <select id="status">
-            <option>All</option>
-            <option>Completed</option>
-            <option>Incomplete</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="search">Search</label>
-          <input id="search" type="text" autoComplete="off" />
-        </div>
-      </section>
-      <ul id="tasks-list">
-        {tasks.map((task) => (
-          <Task
-            key={task.id}
-            id={task.id}
-            description={task.description}
-            completed={task.completed}
-          />
-        ))}
-      </ul>
-      <button id="show-form">+</button>
-    </main>
+    <>
+      <main>
+        <section id="controls">
+          <div>
+            <label htmlFor="status">Status</label>
+            <select id="status">
+              <option>All</option>
+              <option>Completed</option>
+              <option>Incomplete</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="search">Search</label>
+            <input id="search" type="text" autoComplete="off" />
+          </div>
+        </section>
+        <ul id="tasks-list">
+          {tasks.map((task) => (
+            <Task
+              key={task.id}
+              id={task.id}
+              description={task.description}
+              completed={task.completed}
+            />
+          ))}
+        </ul>
+        <button id="show-form" onClick={() => setIsModalOpen(true)}>
+          +
+        </button>
+      </main>
+      <dialog open={isModalOpen} onClick={handleModalClick}>
+        <form onSubmit={handleFormSubmit}>
+          <div className="form-group">
+            <label htmlFor="description">Description</label>
+            <input
+              id="description"
+              name="description"
+              type="text"
+              autoComplete="off"
+              value={taskDescription}
+              onChange={(event) => setTaskDescription(event.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">New Task</button>
+        </form>
+      </dialog>
+    </>
   );
 };
 
